e8fd80ed945288291fc306ea7e1b0aa2d2bcab08
boomdotboom <129564981+boomdotboom@users.noreply.github.com>
2024-04-19 13:14:11 -0500

    Inserting Data

--- Sports.py
+++ Sports.py
@@ -34,12 +34,29 @@ def create_tables():
                                     Position TEXT,
                                     FOREIGN KEY (TeamId) REFERENCES Team(TeamId)
                                 )''')
+        cursor.execute('''INSERT INTO Team (TeamID,Location, Nickname, Conference, Division) VALUES 
+                       (1, 'Arizona', 'Cardinals', 'NFC', 'West'),
+                       (2, 'Alanta','Falcons','NFC', 'South'), (3, 'Carolina','Panthers','NFC','South'),
+                       (4, 'Chicago','Bears','NFC','North'), (5,'Dallas','Cowboys','NFC','East'),
+                       (6,'Detriot','Lions','NFC','North'), (7,'Green Bay','Packers','NFC','North'), 
+                       (8,'Los Angeles','Rams','NFC','West'), (9,'Minnesota','Vikings','NFC', 'North'),
+                       (10,'New Orleans','Saints','NFC','South'), (11,'New York','Giants','NFC','East'),
+                       (12,'Philadelphia','Eagles','NFC','East'),(13,'San Francisco','49ers','NFC','West'),
+                       (14,'Seattle','Seahawks','NFC','West'),(15, 'Tampa Bay','Buccaneers','NFC','South'),
+                       (16, 'Washington','Commanders','NFC','East'),(17, 'Baltimore','Ravens','AFC','North'),
+                       (18,'Buffalo','Bills','AFC','East'),(19,'Cincinnati','Bengals','AFC','North'),
+                       (20,'Cleveland','Browns','AFC','North'),(21,'Denver','Broncos','AFC','South'),
+                       (22,'Houston','Texans','AFC','South'), (23,'Indianapolis','Colts','AFC','South'),
+                       (24,'Jacksonville','Jaguars','AFC','South'), (25,'Kansas City','Chiefs','AFC','West'),
+                       (26,'Las Vegas','Raiders','AFC','West'),(27,'Los Angeles','Chargers','AFC','West'),
+                       (28,'Miami','Dolphins','AFC','East'), (29,'New England','Patriots','AFC','East'),
+                       (30,'New York','Jets','AFC','East'),(31,'Pittsburgh','Steelers','AFC','North'),
+                       (32,'Tennessee','Titans','AFC','South')''')
 def add_game( team1_id, team2_id, score1, score2, date):
-        cursor.execute(f"INSERT INTO Game (TeamId1, TeamId2, Score1, Score2, Date) VALUES ({team1_id},'{team2_id}','{score1}','{score2}')")
+        cursor.execute(f"INSERT INTO Game (TeamId1, TeamId2, Score1, Score2, Date) VALUES ({team1_id},'{team2_id}','{score1}','{score2}','{date}')")
 
 def add_player( team_id, name, position):
-        cursor.execute(f"INSERT INTO Player (TeamId, Name, Position)
-                                VALUES ('{team_id}','{name}', '{position}')")
+        cursor.execute(f"INSERT INTO Player (TeamId, Name, Position) VALUES ('{team_id}','{name}', '{position}')")
 
 def view_players_on_team(team_id):
         cursor.execute(f"SELECT Nickname, PlayerId, Name, Postion  FROM Team,Player WHERE Team.TeamID= Player.TeamID AND TeamId = '{team_id}'")
@@ -65,10 +82,11 @@ def __del__():
         cursor.close()
 
     # Add a game
-add_game(1, 2, 21, 17, '2024-04-15')
+create_tables()
+add_game(1, 29, 21, 17, '2024-04-15')
 
     # Add a player
-add_player(1, 'Tom Brady', 'Quarterback')
+add_player(29, 'Tom Brady', 'Quarterback')
 
     # View players on a team
 print(view_players_on_team(1))
