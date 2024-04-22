3e2796a1e7e44f6e34dd182bac842452882ded2e
boomdotboom <129564981+boomdotboom@users.noreply.github.com>
2024-04-22 15:20:01 -0500

    Droping Tables

---
+++ delete_tables.py
@@ -0,0 +1,13 @@
+import mysql.connector
+import sys
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="aemorton",
+  password="eT5wisee",
+  database= "aemorton"
+) 
+cursor = mydb.cursor()
+def delete_tables():
+    cursor.execute("DROP TABLE Game, Player, Team")
