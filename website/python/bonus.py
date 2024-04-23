fd1e2747b739b3237dd37fca99309e588e787f07
boomdotboom <129564981+boomdotboom@users.noreply.github.com>
2024-04-23 10:20:57 -0500

    Added the rest of the Players

--- website/python/bonus.py
+++ website/python/bonus.py
@@ -12,13 +12,5 @@ cursor = mydb.cursor()
 date= sys.argv[1]
 
 def bonus(date):
-  cursor.execute('''SELECT g.Date, t1.Location, t1.Nickname, p1.Name, p1.Position, g.Score1, 
-                 t2.Location, t2.Nickname,p1.Name, p1.position g.Score2, 
-            IF(g.Score1>g.Score2, Concat(t1.Nickname," Won"),Concat(t1.Nickname," Lost")) AS Result 
-                FROM Game g 
-                    JOIN Team t1 on t1.TeamId= g.TeamId1 
-                    JOIN Team t2 on t2.TeamId= g.TeamId2
-                    JOIN Player p1 on p.TeamId= t1.TeamId
-                    JOIN Player p2 on p.TeamId= t2.TeamId
-                        WHERE g.date= '{date}' ''')
+  cursor.execute(f"SELECT g.Date, t1.Location, t1.Nickname, p1.Name, p1.Position, g.Score1, t2.Location, t2.Nickname,p1.Name, p1.position g.Score2, IF(g.Score1>g.Score2, Concat(t1.Nickname,' Won'),Concat(t1.Nickname,' Lost')) AS Result FROM Game g JOIN Team t1 on t1.TeamId= g.TeamId1 JOIN Team t2 on t2.TeamId= g.TeamId2 JOIN Player p1 on p.TeamId= t1.TeamId JOIN Player p2 on p.TeamId= t2.TeamId WHERE g.date= '{date}' ")
   return cursor.fetchall()
