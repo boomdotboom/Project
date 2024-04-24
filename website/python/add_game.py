29df96bfa7618f0d27fb07c6abaf8fa067141bcd
boomdotboom <129564981+boomdotboom@users.noreply.github.com>
2024-04-24 15:28:04 -0500

    testing

--- website/python/add_game.py
+++ website/python/add_game.py
@@ -3,37 +3,33 @@ import traceback
 import logging
 import Sports
 
-def add_game(gameID, date, team1, score1, team2, score2):
-    try:
-        Sports.open_database()  # open database
-        
-        #next_id = Sports.nextId("Game")
-        
-        values = "'"+ gameID + "','" + date  + "','" +  team1 + "','" + team2 + "','" + score1 + "','" + score2 + "','" + date + "'"
+try:
+    Sports.open_database()  # open database
+    gameID = sys.argv[1]
+    date = sys.argv[2]
+    team1 = sys.argv[3]
+    score1 = sys.argv[4]
+    team2 = sys.argv[5]
+    score2 = sys.argv[6]
+    next_id = Sports.nextId("Game", "gameId" )
+    
+    values = "'"+ str(next_id) + "','" + str(date)  + "','" +  str(team1) + "','" + str(team2) + "','" + str(score1) + "','" + str(score2) + "','" + str(date) + "'"
 
-        Sports.insert("Game", values)
-        res = Sports.executeSelect('SELECT * FROM Game')
-        res = res.split('\n') 
-        print("<br/>" + "<br/>")
-        print("<br/>" + "Table Game after:"+"<br/>" + res[0] + "<br/>"+res[1] + "<br/>")
-        for i in range(len(res)-2):
-            print(res[i+2]+"<br/>")
-            
-        html_content = '''        '''
-        print(html_content)
-        Sports.close_db()  # close db
-    except Exception as e:
-        print("Error in add_game.py")
-        logging.error(traceback.format_exc())
+    Sports.insert("Game", values)
+    res = Sports.executeSelect('SELECT * FROM Game;')
+    res = res.split('\n') 
+    print("<br/>" + "<br/>")
+    print("<br/>" + "Table Game after:"+"<br/>" + res[0] + "<br/>"+res[1] + "<br/>")
+    for i in range(len(res)-2):
+        print(res[i+2]+"<br/>")
+        
+    html_content = '''        '''
+    print(html_content)
+    Sports.close_db()  # close db
+except Exception as e:
+    print("Error in add_game.py")
+    logging.error(traceback.format_exc())
 
 # Parse command line arguments
-gameID = sys.argv[1]
-date = sys.argv[2]
-team1 = sys.argv[3]
-score1 = sys.argv[4]
-team2 = sys.argv[5]
-score2 = sys.argv[6]
 
-# Add the game
-#add_game(gameID, date, team1, score1, team2, score2)
-Sports.add_game(gameID,date,team1,score1,team2,score2)
+#Sports.add_game(gameID,date,team1,score1,team2,score2)
